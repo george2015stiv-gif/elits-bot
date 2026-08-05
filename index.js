65dead912b7e4b1e6385c41e972d3cecabf7bca0
george2015stiv-gif <george2015stiv@gmail.com>
2026-08-05 19:48:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(process.env.PORT || 3000, () => console.log('Keep-alive server ready.'));
 const { 
   Client, 
   GatewayIntentBits, 
