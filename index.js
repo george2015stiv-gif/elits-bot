670922324026fcbf8186b6dd20d5e306de7c4d65
george2015stiv-gif <george2015stiv@gmail.com>
2026-08-06 12:32:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ client.on('ready', async () => {
 
   const rest = new REST({ version: '10' }).setToken(TOKEN);
   try {
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, "await rest.put(Routes.applicationGuildCommands(CLIENT_ID, "ΕΔΩ_ΒΑΛΕ_ΤΟ_SERVER_ID"), { body: commands });"), { body: commands });
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, "1508120097573834752"), { body: commands });
     console.log('✅ Όλες οι εντολές ενεργοποιήθηκαν!');
   } catch (e) { console.error(e); }
 });
